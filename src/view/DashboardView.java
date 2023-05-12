a2638fb8df2db3f101cb1afd662a9532884fc943
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-05-12 18:43:13 +0530

    working on dashboard view

--- src/view/DashboardView.java
+++ src/view/DashboardView.java
@@ -1,18 +1,52 @@
-package view;
-import java.awt.*;
-import controller.DashboardController;
+/*
+ * Created by JFormDesigner on Fri May 12 18:41:09 IST 2023
+ */
 
+package view;
+
+import java.awt.*;
 import javax.swing.*;
 
-public class DashboardView extends AbstractView {
-
+/**
+ * @author ASUS
+ */
+public class DashboardView extends JPanel {
     public DashboardView() {
         initComponents();
-
     }
 
-     public void initComponents() {
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - isuru Yahampath
+
+        //======== this ========
+        setBackground(new Color(0x171717));
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing.
+        border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER
+        , javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font
+        .BOLD ,12 ), java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (
+        new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r"
+        .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+        setLayout(null);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < getComponentCount(); i++) {
+                Rectangle bounds = getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            setMinimumSize(preferredSize);
+            setPreferredSize(preferredSize);
+        }
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
-
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - isuru Yahampath
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
