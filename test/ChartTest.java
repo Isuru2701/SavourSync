66842781deb3e145ab9542026475e755e4a3ca2a
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-05-14 21:23:17 +0530

    running a chart sandbox

---
+++ test/ChartTest.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import javax.swing.*;
+/*
+ * Created by JFormDesigner on Sun May 14 21:22:54 IST 2023
+ */
+
+
+
+/**
+ * @author ASUS
+ */
+public class ChartTest extends JFrame {
+    public ChartTest() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - isuru Yahampath
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - isuru Yahampath
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
