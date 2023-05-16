ab5ceab5e70d16ec81656d2dd2bc3ec79b9bc163
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-05-16 20:11:16 +0530

    one report works

--- src/model/ReportManager.java
+++ src/model/ReportManager.java
@@ -1,9 +1,17 @@
 package model;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperDesignViewer;
 import net.sf.jasperreports.view.JasperViewer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReportManager {
 
@@ -13,10 +21,19 @@ public class ReportManager {
             String query = "SELECT * FROM reservation WHERE DATE(start_datetime) = CURDATE();";
             ResultSet reply = db.query(query);
 
-            JasperReport report = JasperCompileManager.compileReport("C:\\Users\\ASUS\\Documents\\Programs\\SavourSync\\src\\model\\reportTemplates\\testReport.jrxml");
-            JRResultSetDataSource rsds = new JRResultSetDataSource(reply);
-            JasperPrint print = JasperFillManager.fillReport(report, null, rsds);
-            JasperViewer.viewReport(print, false);
+            InputStream input = new FileInputStream(new File("C:\\Users\\ASUS\\Documents\\Programs\\SavourSync\\src\\model\\reportTemplates\\DayReport.jrxml"));
+
+            JasperDesign jd = JRXmlLoader.load(input);
+
+            JRResultSetDataSource resultSetDataSource = new JRResultSetDataSource(reply);
+
+            JasperReport report = JasperCompileManager.compileReport(jd);
+
+            Map<String, Object> parameters = new HashMap<String, Object>();
+            parameters.put("Table", resultSetDataSource);
+
+            JasperPrint print = JasperFillManager.fillReport(report, parameters, new JREmptyDataSource());
+            JasperViewer.viewReport(print);
 
         }
         catch(Exception e) {
